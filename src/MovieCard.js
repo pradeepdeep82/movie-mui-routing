bc4549502003dd0e3e9890f50a46a47fc1b77cda
pradeepdeep82 <pradeepdeep82@gmail.com>
2021-10-24 14:35:27 +0530

    commit

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -51,7 +51,7 @@ export function MovieCard({
     .then(data=>data.json())
     .then(mvs=>setMovies(mvs))
   };
-  useEffect(getMovies);
+  useEffect(getMovies,[setMovies]);
   return (
     <div className="MovieCard">
      
