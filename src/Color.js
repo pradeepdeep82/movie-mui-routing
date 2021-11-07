3d372eb58a03807c7db9a4023e11603073466fdf
pradeepdeep82 <pradeepdeep82@gmail.com>
2021-11-07 14:33:09 +0530

    usereducer done

--- src/Color.js
+++ src/Color.js
@@ -1,33 +1,53 @@
 import React from 'react';
 import { useReducer } from "react";
 
-const initialState={color:""};
-const reducer= (state,action)=>{
-  switch (action.type){
-    case"inputColor":{
-      return {color: action.payload};
+//Single source of truth - It contains all data
+
+const initialState = { count: 1, color: "red" };
+const reducer = (state, action) => {
+  // switch, state - current value
+  switch (action.type) {
+    case "increment": {
+      return { ...state, count: state.count + 1 };
     }
-
-  default:
-    return state;
+    case "decrement": {
+      return { ...state, count: state.count - 1 };
+    }
+    case "reset": {
+      return { color:state.color, count: action.payload };
+    }
+    case "color": {
+      return { ...state, color: action.payload };
+    }
+    default:
+      return state;
   }
-}
-export function Color() {
-  const [state, dispatch]=useReducer(reducer, initialState);
-  const styles={
-    backgroundColor: state.color,
-    fontSize:"1.5rem",
-    fontWeight:'bold'
-  };
-  return (
-    <div>
-      <br/>
-      <input
-        style={styles}
-        placeholder="Enter a color" 
-        onChange={(event)=>dispatch({ type:"inputColor", payload:event.target.value })}/>
-      {/* <button >Add color</button> */}
+};
 
+export  function Color() {
+  const [state, dispatch] = useReducer(reducer, initialState); // subscribing
+  return (
+    <div className="App">
+      {/* updated store value */}
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      {/* use the same useReducer to compelete task */}
+      <button onClick={() => dispatch({ type: "reset", payload: 0 })}>
+        Reset
+      </button>
+      <input
+        value={state.color}
+        style={{ background: state.color }}
+        onChange={(event) =>
+          dispatch({ type: "color", payload: event.target.value })
+        }
+      />
     </div>
   );
 }
+
+// { type: "increment" } - action
+
+// flow
+// View -> dispatch action -> Reducer -> Store -> View (new data)
